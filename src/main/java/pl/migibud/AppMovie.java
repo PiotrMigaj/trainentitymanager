0d9ee13ba92d1e253a15a6e7649550b66277150b
Piotr Migaj praca <piotr.migaj@pg-projekt.pl>
2023-01-18 16:30:37 +0100

    next commit

--- src/main/java/pl/migibud/AppMovie.java
+++ src/main/java/pl/migibud/AppMovie.java
@@ -1,15 +1,8 @@
 package pl.migibud;
 
-import jakarta.persistence.EntityNotFoundException;
 import org.hibernate.SessionFactory;
-import pl.migibud.genre.Genre;
-import pl.migibud.genre.GenreDao;
-import pl.migibud.genre.GenreDaoImpl;
-import pl.migibud.movie.Movie;
 import pl.migibud.movie.MovieDao;
 import pl.migibud.movie.MovieDaoImpl;
-
-import java.util.List;
 
 public class AppMovie {
     public static void main(String[] args) {
